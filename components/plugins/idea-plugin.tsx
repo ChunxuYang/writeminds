7ceb22f3f0161e4494f0309be13446eae85be15a
QGY511 <qgy511@outlook.com>
2023-06-01 23:02:03 -0700

    feat: Idea function plugin

---
+++ components/plugins/idea-plugin.tsx
@@ -0,0 +1,227 @@
+import {
+    $createNodeSelection,
+    $createRangeSelection,
+    $createTextNode,
+    $getNodeByKey,
+    $getRoot,
+    $getSelection,
+    $isRangeSelection,
+    $nodesOfType,
+    $setSelection,
+    COMMAND_PRIORITY_HIGH,
+    INSERT_PARAGRAPH_COMMAND,
+    KEY_SPACE_COMMAND,
+    KEY_TAB_COMMAND,
+    LexicalCommand,
+    RangeSelection,
+    TextNode,
+    createCommand,
+  } from "lexical";
+  import { AiFillRobot } from "react-icons/ai";
+  
+  import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+  import { useEffect, useState } from "react";
+  import { Button, Input, Modal, Loading } from "@nextui-org/react";
+  import { mergeRegister } from "@lexical/utils";
+  //import { $createSuggestionNode } from "~/components/plugins/custom-nodes/suggestion-node";
+  import { motion } from "framer-motion";
+  
+  export const GET_IDEA_COMMAND: LexicalCommand<string> = createCommand();
+  
+  // trigger command is /idea
+  const trigger_command = "/idea ";
+  
+  export default function IdeaPlugin() {
+    const [editor] = useLexicalComposerContext();
+    const [modalOpen, setModalOpen] = useState(false);
+    const [ideaLoading, setIdeaLoading] = useState(false);
+  
+    const [currentTextNodeKey, setCurrentTextNodeKey] = useState("");
+  
+    const [question, setQuestion] = useState("");
+  
+    const [ideas, setIdeas] = useState<string[]>([]);
+  
+    const [selectedIdea, setSelectedIdea] = useState<string | null>(
+      null
+    );
+  
+    function closeHandler() {
+      setModalOpen(false);
+      setIdeaLoading(false);
+      setQuestion("");
+      setSelectedIdea(null);
+      setIdeas([]);
+      editor.focus();
+      editor.update(() => {});
+    }
+  
+    useEffect(() => {
+      return mergeRegister(
+        editor.registerNodeTransform(TextNode, (node) => {
+          if (node.__text.endsWith(trigger_command)) {
+            editor.update(() => {
+              node.spliceText(
+                node.__text.length - trigger_command.length,
+                node.__text.length,
+                " "
+              );
+  
+              setCurrentTextNodeKey(node.__key);
+  
+              setModalOpen(true);
+  
+              return true;
+            });
+          }
+  
+          return true;
+        }),
+  
+        editor.registerCommand<string>(
+          GET_IDEA_COMMAND,
+          () => {
+            setModalOpen(true);
+            return true;
+          },
+          COMMAND_PRIORITY_HIGH
+        )
+      );
+    }, [editor]);
+  
+    const [totalContent, setTotalContent] = useState("");
+  
+    // return null;
+  
+    return (
+      <Modal open={modalOpen} onClose={closeHandler}>
+        <Modal.Header>/Idea</Modal.Header>
+        <Modal.Body>
+          <Input
+            placeholder="Type anything to ask for an idea"
+            rounded
+            aria-label="Type anything to ask for an idea"
+            bordered
+            className="rounded"
+            clearable
+            contentRightStyling={false}
+            size="md"
+            contentRight={
+              ideaLoading ? (
+                <Loading
+                  type="points"
+                  color="primary"
+                  size="sm"
+                  className="mr-2"
+                />
+              ) : (
+                <motion.button
+                  className="rounded-full bg-primary p-[5px] mr-2 cursor-pointer text-white z-50"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  onClick={async () => {
+                    setIdeaLoading(true);
+                    editor.getEditorState().read(() => {
+                      const rootNode = $getRoot();
+                      const allContent = rootNode.getTextContent();
+                      setTotalContent(allContent);
+                      fetch("/api/idea", {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                          text: allContent,
+                        }),
+                      })
+                        .then((res) => res.json())
+                        .then((data) => {
+                          setIdeaLoading(false);
+                          setIdeas(data);
+                        })
+                        .catch((error) => {
+                          setIdeaLoading(false);
+                          setIdeas([]);
+                          console.error("Error:", error);
+                        });
+                    });
+                  }}
+                >
+                  <AiFillRobot />
+                </motion.button>
+              )
+            }
+          />
+  
+          <motion.div
+            className="rounded bg-gray-100 p-3 mt-2 space-y-2 flex flex-col"
+            layout
+            layoutRoot
+            // initial={{ opacity: 0 }}
+            // animate={{
+            //   height: ideaLoading ? "auto" : 0,
+            //   opacity: ideaLoading ? 1 : 0,
+            // }}
+            style={{
+              display: ideas.length > 0 ? "block" : "none",
+            }}
+          >
+            {ideas.map((idea, index) => (
+              <motion.div
+                key={index}
+                layout
+                className="rounded bg-white p-2 cursor-pointer hover:bg-gray-200"
+                onClick={() => {
+                  if (!selectedIdea) {
+                    setSelectedIdea(idea);
+                    setIdeaLoading(true);
+                    fetch("/api/idea/detail", {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({
+                        text: totalContent,
+                        choice: idea,
+                      }),
+                    })
+                      .then((res) => res.json())
+                      .then((data) => {
+                        setIdeas([data]);
+                        setIdeaLoading(false);
+                      })
+                      .catch((error) => {
+                        setIdeaLoading(false);
+                        // setIdeas([]);
+                        setSelectedIdea(null);
+                        console.error("Error:", error);
+                      });
+                  } else {
+                    editor.update(() => {
+                      // add the idea to cursor position
+                      const currentNode = $getNodeByKey(currentTextNodeKey);
+  
+                      if (!currentNode) return;
+  
+                      currentNode.spliceText(
+                        currentNode.__text.length,
+                        currentNode.__text.length,
+                        idea
+                      );
+  
+                      // close the modal
+                      closeHandler();
+                    });
+                  }
+                }}
+              >
+                {idea}
+              </motion.div>
+            ))}
+          </motion.div>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    );
+  }
+  
