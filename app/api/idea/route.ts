2681599536be899172d3aa198fbe9a1b98439db8
QGY511 <qgy511@outlook.com>
2023-06-01 22:58:28 -0700

    idea function (updated route.ts)

---
+++ app/api/idea/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const getContext = (text: string): Array<ChatCompletionRequestMessage> => {
+  const context: Array<ChatCompletionRequestMessage> = [
+    {
+      role: "user",
+      content: `This is my essay now: ${text} \n\n, I want to know how to brainstorm it, return me 2 - 3 new ideas based on my current essay and end with ellipsis.\n return should be consise, each containing below 5-10 words. \n return example:
+                Idea 1: xxxx | Idea 2: yyyy | Idea 3: zzzz.`,
+    },
+  ];
+  return context;
+};
+
+export async function POST(request: Request) {
+  const { text } = await request.json();
+  const res = await openai.createChatCompletion({
+    // model: "text-davinci-003",
+    // prompt: "This is a new essay beginning: \n" + text,
+    // stop: ["\n"],
+    model: "gpt-3.5-turbo",
+    messages: getContext(text),
+  });
+
+  const message = res.data.choices[0].message?.content;
+
+  if (!message || message.length === 0) {
+    return NextResponse.json([]);
+  }
+
+  // message like: Talk about xxxx | Talk about yyyy | Talk about zzzz
+  const messages = message.split("...").map((m) => m.trim()).filter((m) => m !== "");
+
+  return NextResponse.json(messages);
+}
